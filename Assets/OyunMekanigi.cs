2397cd4b5fda7056f356a3464d6e8f9f69af44be
Taha <55713590+TahaMetin@users.noreply.github.com>
2020-12-01 00:26:05 +0300

    project

---
+++ Assets/OyunMekanigi.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class OyunMekanigi : MonoBehaviour {
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] int tahmin;
+    [SerializeField] TextMeshProUGUI TahminMetni;
+
+	// Use this for initialization
+	void Start () {
+        OyununBaslangici();
+	}
+	
+    public void OyununBaslangici()
+    {
+        max = max + 1;
+        SonrakiTahmin();
+
+    }
+
+    public void arttir()
+    {
+        min = tahmin;
+        SonrakiTahmin();
+    }
+
+    public void azalt()
+    {
+        max = tahmin;
+        SonrakiTahmin();
+    }
+
+    public void SonrakiTahmin()
+    {
+        //tahmin = (min + max) / 2;
+        tahmin = Random.Range(min, max);
+        TahminMetni.text = tahmin.ToString();
+    }
+	
+}
---
+++ Assets/SahneKontroluScripti.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SahneKontroluScripti : MonoBehaviour {
+
+    public void SonrakiSahne()
+    {
+        int MevcutSahneninİndexi = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(MevcutSahneninİndexi + 1);
+    }
+
+    public void OyunSahnesineYönlen()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OyundanCik()
+    {
+        Application.Quit();
+    }
+
+}
+
